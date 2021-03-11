96c8be24eb858364ccfcb5885b3e5d6eb8214937
imad <imad.elfetouh@hotmail.com>
2021-03-11 13:43:45 +0100

    hook setup

---
+++ src/pages/simulation/index.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import './index.css'
+
+export default function Simulation() {
+
+    return (
+        <div id="simulationWrapper">
+            <div id="simulationHeader">
+                <div>
+                    <Button variant="success">Success</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
