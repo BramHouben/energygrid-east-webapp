485e09422d92d5c941c8427249c441625ea48870
Robert8034 <53059207+Robert8034@users.noreply.github.com>
2021-03-16 11:35:25 +0100

    Update src/router/router.js

--- src/router/router.js
+++ src/router/router.js
@@ -11,8 +11,8 @@ const Router = () => (
     <Switch>
       <Route path={paths.Root} exact component={Dashboard} />
       <Route path={paths.Simulation} exact component={Simulation} />
-      <Route path={paths.Account} component={Account} />
-      <Route path={paths.Login} component={Login}/>
+      <Route path={paths.Account} exact component={Account} />
+      <Route path={paths.Login} exact component={Login}/>
     </Switch>
   </BrowserRouter>
 );
