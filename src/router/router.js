37477a7d76912883a667626cbcf53047510bc46f
Frits van Lieshout <fritske05@gmail.com>
2021-03-16 10:16:56 +0100

    Fixed requested changes

--- src/router/router.js
+++ src/router/router.js
@@ -4,11 +4,9 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import paths from "services/shared/router-paths";
 import Dashboard from "../pages/dashboard";
 import Simulation from "../pages/simulation";
-import Header from "components/shared/header";
 
 const Router = () => (
   <BrowserRouter>
-  <Header pageName="Dashboard" />
     <Switch>
       <Route path={paths.Root} exact component={Dashboard} />
       <Route path={paths.Simulation} exact component={Simulation} />
