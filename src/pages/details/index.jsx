e0e5eec715f36ef2affd4f9a531d0bef45b84ac9
Frits van Lieshout <fritske05@gmail.com>
2021-03-23 15:45:52 +0100

    Added search bar by table

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -3,70 +3,108 @@ import "./index.css";
 import Header from "components/shared/header";
 import { Table } from "react-bootstrap";
 import data from "../../data/solarparks-east.json";
-import Maps from "components/shared/maps";
-import { withGoogleMap, withScriptjs } from "react-google-maps";
+import SearchBar from "components/shared/inputs/search";
+//import Maps from "components/shared/maps";
+
+import { ArrowUp, ArrowDown } from "react-bootstrap-icons";
 
 export default class Details extends React.Component {
   constructor() {
     super();
     this.state = {
       solarParks: [],
+      filteredSolarParks: [],
+      input: "",
     };
   }
 
   componentDidMount() {
-    this.setState({ solarParks: data.solarParks });
+    this.setState({
+      solarParks: data.solarParks,
+      filteredSolarParks: data.solarParks,
+    });
+
+    window.addEventListener("search-input", (e) => {
+      console.log(e);
+      this.setFilters(e.detail.input);
+    });
   }
 
-  handleOrderClick(direction, col) {
-    const solarParks = this.state.solarParks;
+  setFilters(value) {
+    const filteredSolarParks = this.state.solarParks;
 
-    // if (direction === "ASC") {
-    //   solarParks.sort((a, b) => {
-    //     return a[col] - b[col];
-    //   });
-    // }
-    //TODO
-
-    let newSolarParks = {};
-    if (direction === "ASC") {
-      newSolarParks = solarParks.sort((a, b) =>
-        a[col] > b[col] ? 1 : b[col] > a[col] ? -1 : 0
-      );
+    if (value === null || value === "") {
+      this.setState({ input: value, filteredSolarParks: filteredSolarParks });
     } else {
-      newSolarParks = solarParks.sort((a, b) =>
-        a[col] < b[col] ? 1 : b[col] < a[col] ? -1 : 0
-      );
-    }
+      const filtered = filteredSolarParks.filter((solarPark) => {
+        console.log(solarPark, value);
+        return solarPark.location.includes(value)
+          ? solarPark.location.includes(value)
+          : solarPark.applicant.includes(value)
+          ? solarPark.applicant.includes(value)
+          : null;
+      });
 
-    this.setState({ solarParks: newSolarParks });
+      console.log(filtered);
+      this.setState({ input: value, filteredSolarParks: filtered });
+    }
   }
 
   render() {
-    let { solarParks } = this.state;
-    const MapWrapped = withGoogleMap(Maps);
+    let { filteredSolarParks, input } = this.state;
+    //const MapWrapped = withGoogleMap(Maps);
 
     return (
       <div>
         <Header pageName="details" />
         <div id="details-body">
+          <div id="search-bar">
+            <SearchBar input={input} />
+          </div>
           <Table striped bordered hover responsive="sm">
             <thead>
               <tr>
-                <th>ID</th>
-                <th>Aanvrager</th>
-                <th>Adres</th>
-                <th>Postcode</th>
-                <th>Plaats</th>
-                <th>Provincie</th>
-                <th>Power</th>
-                <th>Max</th>
-                <th>Gerealiseerd op</th>
+                <th>
+                  ID
+                  <ArrowUp id="th-icon" />
+                </th>
+                <th>
+                  Aanvrager
+                  <ArrowUp id="th-icon" />
+                </th>
+                <th>
+                  Adres
+                  <ArrowUp id="th-icon" />
+                </th>
+                <th>
+                  Postcode
+                  <ArrowUp id="th-icon" />
+                </th>
+                <th>
+                  Plaats
+                  <ArrowUp id="th-icon" />
+                </th>
+                <th>
+                  Provincie
+                  <ArrowUp id="th-icon" />
+                </th>
+                <th>
+                  Power
+                  <ArrowUp id="th-icon" />
+                </th>
+                <th>
+                  Max
+                  <ArrowUp id="th-icon" />
+                </th>
+                <th>
+                  Gerealiseerd op
+                  <ArrowUp id="th-icon" />
+                </th>
               </tr>
             </thead>
             <tbody>
-              {!!solarParks &&
-                solarParks.map((solarPark) => (
+              {!!filteredSolarParks &&
+                filteredSolarParks.map((solarPark) => (
                   <tr key={solarPark.id}>
                     <td>{solarPark.id}</td>
                     <td>{solarPark.applicant}</td>
@@ -81,12 +119,6 @@ export default class Details extends React.Component {
                 ))}
             </tbody>
           </Table>
-          <MapWrapped
-            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.GOOGLE_MAPS_API_KEY}`}
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `100%` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-          />
         </div>
       </div>
     );
