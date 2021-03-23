eaa2392041df140a779b641636131a71a628ea98
Frits van Lieshout <fritske05@gmail.com>
2021-03-23 11:12:54 +0100

    Added map

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -3,6 +3,8 @@ import "./index.css";
 import Header from "components/shared/header";
 import { Table } from "react-bootstrap";
 import data from "../../data/solarparks-east.json";
+import Maps from "components/shared/maps";
+import { withGoogleMap, withScriptjs } from "react-google-maps";
 
 export default class Details extends React.Component {
   constructor() {
@@ -19,22 +21,30 @@ export default class Details extends React.Component {
   handleOrderClick(direction, col) {
     const solarParks = this.state.solarParks;
 
-    if (direction === "ASC") {
-      solarParks.sort((a, b) => {
-        return a[col] - b[col];
-      });
+    // if (direction === "ASC") {
+    //   solarParks.sort((a, b) => {
+    //     return a[col] - b[col];
+    //   });
+    // }
+    //TODO
 
-      console.log(solarParks);
+    let newSolarParks = {};
+    if (direction === "ASC") {
+      newSolarParks = solarParks.sort((a, b) =>
+        a[col] > b[col] ? 1 : b[col] > a[col] ? -1 : 0
+      );
+    } else {
+      newSolarParks = solarParks.sort((a, b) =>
+        a[col] < b[col] ? 1 : b[col] < a[col] ? -1 : 0
+      );
     }
 
-    let newSolarParks = solarParks.sort((a, b) =>
-      a[col] > b[col] ? 1 : b[col] > a[col] ? -1 : 0
-    );
-    console.log(newSolarParks);
+    this.setState({ solarParks: newSolarParks });
   }
 
   render() {
     let { solarParks } = this.state;
+    const MapWrapped = withGoogleMap(Maps);
 
     return (
       <div>
@@ -44,9 +54,7 @@ export default class Details extends React.Component {
             <thead>
               <tr>
                 <th>ID</th>
-                <th onClick={() => this.handleOrderClick("ASC", "applicant")}>
-                  Aanvrager
-                </th>
+                <th>Aanvrager</th>
                 <th>Adres</th>
                 <th>Postcode</th>
                 <th>Plaats</th>
@@ -73,6 +81,12 @@ export default class Details extends React.Component {
                 ))}
             </tbody>
           </Table>
+          <MapWrapped
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.GOOGLE_MAPS_API_KEY}`}
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `100%` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
         </div>
       </div>
     );
