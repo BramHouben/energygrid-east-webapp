c9b736120f39f115535091cc080b6f638c3ea8df
Vincent Darwinkel <vincent.darwinkel@hotmail.nl>
2021-04-06 13:26:37 +0200

    Fix sonarlint code smell

--- src/services/registration.js
+++ src/services/registration.js
@@ -2,6 +2,6 @@ import ApiActions from "services/shared/api/ApiActions";
 import { Post } from "./shared/api/Api";
 import { sendRequest } from "./shared/api/api-middleware";
 
-export const Register = async (json) => {
-  return await sendRequest(() => Post(ApiActions.User, json));
+export const Register = (json) => {
+  return sendRequest(() => Post(ApiActions.User, json));
 };
