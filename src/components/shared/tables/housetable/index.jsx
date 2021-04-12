4259e03ed6c6abda5ba334a811167e7bafe1626f
Houben,Bram B <bram.houben@student.fontys.nl>
2021-04-12 16:20:23 +0200

    region now working with paging and has a map

--- src/components/shared/tables/housetable/index.jsx
+++ src/components/shared/tables/housetable/index.jsx
@@ -1,94 +1,137 @@
 import "./index.css";
-import React, { Component } from "react";
 import Axios from "axios";
-import BootstrapTable from "react-bootstrap-table-next";
-import paginationFactory from "react-bootstrap-table2-paginator";
 import ApiActions from "services/shared/api/ApiActions";
+import React, { Component } from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory, {
+  PaginationProvider,
+  PaginationListStandalone,
+} from "react-bootstrap-table2-paginator";
+
+const RemotePagination = ({
+  data,
+  page,
+  sizePerPage,
+  onTableChange,
+  totalSize,
+}) => (
+  <div>
+    <PaginationProvider
+      pagination={paginationFactory({
+        custom: true,
+        page,
+        sizePerPage,
+        totalSize,
+      })}
+    >
+      {({ paginationProps, paginationTableProps }) => (
+        <div>
+          <div>
+            <PaginationListStandalone {...paginationProps} />
+          </div>
+          <BootstrapTable
+            remote
+            keyField='hash'
+            data={data}
+            columns={[
+              {
+                dataField: "hash",
+                hidden: true,
+              },
+              {
+                dataField: "street",
+                text: "street",
+                sort: true,
+              },
+              {
+                dataField: "number",
+                text: "number",
+                sort: true,
+              },
+              {
+                dataField: "postcode",
+                text: "postcode",
+                sort: true,
+              },
+              {
+                dataField: "region",
+                text: "region",
+                sort: true,
+              },
+              {
+                dataField: "coordinates",
+                text: "coordinates",
+                sort: true,
+              },
+            ]}
+            onTableChange={onTableChange}
+            {...paginationTableProps}
+          />
+        </div>
+      )}
+    </PaginationProvider>
+  </div>
+);
 
 export default class Housetable extends Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [],
-      page: 0,
-      totalsize: 0,
+      allpoints: [],
+      page: 1,
+      totalSize: 0,
       sizePerPage: 10,
+      updatelist: this.props.updatelist,
     };
   }
-  columns = [
-    {
-      dataField: "hash",
-      hidden: true,
-    },
-    {
-      dataField: "street",
-      text: "street",
-      sort: true,
-    },
-    {
-      dataField: "number",
-      text: "number",
-      sort: true,
-    },
-    {
-      dataField: "postcode",
-      text: "postcode",
-      sort: true,
-    },
-    {
-      dataField: "region",
-      text: "region",
-      sort: true,
-    },
-    {
-      dataField: "coordinates",
-      text: "coordinates",
-      sort: true,
-    },
-  ];
-  // componentDidUpdate(PrevProps) {
-  //   const newprops = this.props;
-  //   if (PrevProps.totalsize !== newprops.totalsize) {
-  //     this.fetchData();
-  //   }
-  // }
+  handleTableChange = async (type, { page, sizePerPage }) => {
+    setTimeout(() => {
+      this.fetchData(page, sizePerPage);
+      this.setState(() => ({
+        page,
+        sizePerPage,
+      }));
+    }, 1000);
+  };
 
-  componentDidMount() {
-    this.fetchData();
-  }
   async fetchData(
     page = this.state.page,
-    sizePerPage = this.state.sizePerPage
+    sizeperPage = this.state.sizePerPage
   ) {
     await Axios.get(ApiActions.StreetInfo, {
       params: {
         streetname: "Josephine Bakerstraat",
         page: page,
       },
-    }).then((result) => {
-      this.setState({
-        items: result.data.houses,
-        totalsize: result.data.totalStreet,
+    })
+      .then((result) => {
+        this.state.updatelist(result.data.houses);
+
+        this.setState({
+          items: result.data.houses,
+          totalsize: result.data.totalStreet,
+        });
+      })
+      .catch((result) => {
+        console.log("error loading results");
       });
-    });
+  }
+
+  componentDidMount() {
+    this.fetchData(this.state.page);
   }
   render() {
-    console.log(this.state.totalsize);
+    let { items, sizePerPage, page, totalsize } = this.state;
     return (
       <div>
-        {this.state.items.length > 0 ? (
-          <BootstrapTable
-            keyField='hash'
-            data={this.state.items}
-            columns={this.columns}
-            striped
-            hover
-            condensed
-            pagination={paginationFactory({
-              totalSize: this.state.totalsize,
-            })}
-          />
-        ) : null}
+        <RemotePagination
+          data={items}
+          page={page}
+          sizePerPage={sizePerPage}
+          totalSize={totalsize}
+          onTableChange={this.handleTableChange}
+        />
       </div>
     );
   }
--- src/pages/region/index.jsx
+++ src/pages/region/index.jsx
@@ -1,11 +1,65 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Container } from "react-bootstrap";
 import RegionTable from "components/shared/tables/housetable";
 import "./index.css";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import Header from "components/shared/header";
 import FilterHeader from "components/shared/filter-header";
+
 export default class Region extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      page: 0,
+      totalsize: 0,
+      items: [],
+      viewport: {
+        latitude: 52.18889722321286,
+        longitude: 6.124005761032457,
+        width: "83.5vw",
+        height: "83.5vh",
+        zoom: 8,
+      },
+      selectedhouses: {},
+    };
+    this.updatelist = this.updatelist.bind(this);
+    // this.fetchData = this.fetchData.bind(this);
+  }
+
+  async updatelist(items) {
+    this.setState({
+      items: items,
+    });
+    console.log(this.state.items);
+  }
+
+  // componentDidMount() {
+  //   <Streetmap data={this.state.items}></Streetmap>;
+  // }
+  // async fetchData(page = this.state.page, sizePerPage = 10) {
+  //   Axios.get(ApiActions.StreetInfo, {
+  //     params: {
+  //       streetname: "Josephine Bakerstraat",
+  //       page: page,
+  //     },
+  //   })
+  //     .then((result) => {
+  //       this.setState({
+  //         items: result.data.houses,
+  //         totalsize: result.data.totalStreet,
+  //       });
+  //     })
+  //     .catch((result) => {
+  //       console.log("error loading results");
+  //     });
+  // }
   render() {
+    if (!this.state.items) {
+      return <div />;
+    }
+    var updatelist = this.updatelist;
+
     return (
       <div>
         <div className='header-wrapper'>
@@ -14,7 +68,63 @@ export default class Region extends Component {
         </div>
         <div className='content'>
           <Container className='loginContainer'>
-            <RegionTable />
+            <RegionTable updatelist={updatelist.bind(this)} />
+            {this.state.items && this.state.items.length > 0 ? (
+              <ReactMapGL
+                {...this.state.viewport}
+                mapboxApiAccessToken={process.env.REACT_APP_MAP_BOX_TOKEN}
+                onViewportChange={(viewport) => {
+                  this.setState({ viewport: viewport });
+                }}
+              >
+                {this.state.items.map((houses) => (
+                  <Marker
+                    key={houses.id}
+                    latitude={houses.coordinates[1]}
+                    longitude={houses.coordinates[0]}
+                  >
+                    <button
+                      className='marker-btn'
+                      onClick={(e) => {
+                        e.preventDefault();
+                        this.setState({
+                          selectedhouses: houses,
+                        });
+                      }}
+                    >
+                      <img src='/assets/house/house.png' alt='house-icon' />
+                    </button>
+                  </Marker>
+                ))}{" "}
+                {this.selectedhouses ? (
+                  <Popup
+                    className='popup'
+                    latitude={this.selectedhouses.coordinates[1]}
+                    longitude={this.selectedhouses.coordinates[0]}
+                    onClose={() => {
+                      this.setState({
+                        selectedhouses: null,
+                      });
+                    }}
+                  >
+                    <div id='popup-items'>
+                      <h4>{this.selectedhouses.street}</h4>
+                      <p>
+                        {this.selectedhouses.region}{" "}
+                        {this.selectedhouses.postcode}{" "}
+                        {this.selectedhouses.number}{" "}
+                      </p>
+                      <p>
+                        <b>Province: </b>
+                        {this.selectedhouses.region}
+                      </p>
+                    </div>
+                  </Popup>
+                ) : null}
+              </ReactMapGL>
+            ) : (
+              <div></div>
+            )}
           </Container>
         </div>
       </div>
