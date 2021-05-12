f2ba544a2229c339a51d95a7d2d57d8d70d6caf4
Vincent Darwinkel <vincent.darwinkel@hotmail.nl>
2021-05-12 13:34:40 +0200

    Added energymarket page

---
+++ src/pages/energy-market/index.jsx
@@ -0,0 +1,61 @@
+import Header from "components/shared/header";
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
+import { withTranslation } from "react-i18next";
+import { BuyOrSellEnergy } from "services/shared/energy-market";
+import { getFormDataInJson } from "services/shared/form-data-helper";
+import "./index.css";
+
+class EnergyMarket extends Component {
+  constructor() {
+    super();
+    this.state = {
+      price: 0,
+    };
+  }
+
+  onEnergyChange = (e) => {
+    const price = Math.abs(e.target.value * 0.22);
+    this.setState({ price });
+  };
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = getFormDataInJson(e);
+    formData.region = "east";
+    await BuyOrSellEnergy(formData);
+  };
+
+  render() {
+    const { t } = this.props;
+    const { price } = this.state;
+
+    return (
+      <div>
+        <Header pageName={t("pageName")} />
+        <div className="content">
+          <Form onSubmit={this.onSubmit} id="energy-market-form">
+            <Form.Group>
+              <Form.Label>{t("sell-buy-label")}</Form.Label>
+              <Form.Control
+                name="energyQuantity"
+                onChange={this.onEnergyChange}
+                defaultValue="0"
+                required
+                type="number"
+              />
+              <small>{t("sell-by-description")}</small>
+            </Form.Group>
+            <label>
+              {t("price")} {price}
+            </label>
+            <br />
+            <Button>{t("submit-btn")}</Button>
+          </Form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withTranslation("energyMarket")(EnergyMarket);
--- src/services/shared/router-paths.js
+++ src/services/shared/router-paths.js
@@ -3,12 +3,13 @@ const paths = {
   Root: "/",
   Simulation: "/simulation/",
   Account: "/account/",
+  EnergyMarket: "/energy-market/",
   Login: "/login/",
   Forecast: "/forecast/",
   Map: "/map/",
   Region: "/region/",
   Registration: "/registration/",
-  Production: '/production/',
+  Production: "/production/",
   TermsAndServices: "/terms-and-services/",
 };
 
