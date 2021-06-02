150aa83998f01a250ae058be1231f4528b83ad5b
Houben,Bram B <bram.houben@student.fontys.nl>
2021-06-02 20:09:24 +0200

    test fix

--- src/tests/account-role.test.js
+++ src/tests/account-role.test.js
@@ -8,6 +8,7 @@ test("Check if roles exists", () => {
     Customer: "CUSTOMER",
     LargeScaleCustomer: "LARGE_SCALE_CUSTOMER",
     Admin: "ADMIN",
+    GridOperator: "GRID_OPERATOR",
     UtilityCompany: "UTILITY_COMPANY",
     ResponsibleParty: "RESPONSIBLE_PARTY",
   });
